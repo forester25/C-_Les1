14a7b3553e8dbdd2573069bcce2774b1237ff7f8
Egor <uvarov-97@mail.ru>
2022-09-19 22:20:08 +0300

    If Else string маша

---
+++ lesson1_ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите своё имя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, Это же МАША");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}  
