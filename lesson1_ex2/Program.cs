d127edcec36c0835b252b818c75f82431dc7eb24
Egor <uvarov-97@mail.ru>
2022-09-19 21:45:26 +0300

    Оператор New Random().Next(min,max)

---
+++ lesson1_ex2/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1,10); //1,2,3,4...9
+int numberB = new Random().Next(1,10);
+int result = numberA + numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(result);
